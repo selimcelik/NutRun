816e718396dc61c8b3b59b9f309597b7d5b81e0e
Selim Çelik <selim.celik@mobilistbilisim.com>
2022-06-30 15:01:21 +0300

    finish commit

---
+++ Assets/Scenes/test.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+
+    
+    void Start()
+    {
+
+
+    }
+
+
+    void Update()
+    {
+
+
+
+
+    }
+
+
+
+}
